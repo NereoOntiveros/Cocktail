43426329eaf9cfb22ff6d7483e519e7ae8d7cb44
Nereo Ontiveros <nerontiveros@gmail.com>
2020-11-12 20:26:16 +0000

    Add details to detail fragment

--- app/src/main/java/com/nereoontiveros/cocktail/ui/CocktailDetailFragment.kt
+++ app/src/main/java/com/nereoontiveros/cocktail/ui/CocktailDetailFragment.kt
@@ -6,8 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
 import com.nereoontiveros.cocktail.R
 import com.nereoontiveros.cocktail.data.model.Drink
+import kotlinx.android.synthetic.main.fragment_cocktail_detail.*
 
 
 class CocktailDetailFragment : Fragment() {
@@ -30,5 +32,11 @@ class CocktailDetailFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_cocktail_detail, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Glide.with(requireContext()).load(drink.image).centerCrop().into(image_drink)
+        tv_drink_title.text=drink.name
+        tv_drink_description.text=drink.description
 
+    }
 }
